0a566b7fa8fd351da81ebc782b15f9dd4713b34a
Chorer <zcl82806932@163.com>
2019-04-01 12:02:13 +0800

    Add hightlight for toc lists

--- PureBlue/source/js/toc.js
+++ PureBlue/source/js/toc.js
@@ -1,4 +1,4 @@
-// TOC定位
+// TOC fixed
 var oDiv = document.getElementsByClassName("toc")[0],H = 0,Y = oDiv;        
 while (Y) {
     H += Y.offsetTop; 
@@ -14,7 +14,7 @@ window.onscroll = function()
     }
 };
 
-// 生成目录文本li
+// 'catalog'text
 var catalog = document.getElementById("post-catalog-text"); 
 var txt = document.createElement("li");
 txt.className="real-catalog-text";
@@ -23,14 +23,35 @@ oDiv.insertBefore(txt,oDiv.children[0]);
 
 
 
-// 生成返回顶部文本li
+// 'to the top'text
 var toTop = document.getElementById("post-toTop-text"); 
 var topper =document.createElement("li");
 topper.className="real-toTop-text";
 topper.innerHTML='<i class="fa fa-chevron-circle-up"></i>'+toTop.innerHTML;
 oDiv.appendChild(topper);
 
-
+// catalog highlight
+var $ol = $('.toc');         
+var titleList = $('.post-content').find('h1,h2,h3,h4,h5,h6');
+$(window).scroll(function (e) {
+    var top = $(document).scrollTop();
+    var currentID = '';
+    titleList.each(function () {
+        var $this = $(this);
+        var itemTop = $this.offset().top;
+        if (top > itemTop - 300) {
+            currentID = '#' + $this.attr('id');
+        } else {
+          return false;
+        }
+  });
+    var currentLink = $ol.find('.actived');
+    if (currentID && currentLink.attr('href') !== currentID) {
+         currentLink.removeClass('actived');
+         var aList = $ol.find("[href='" + currentID + "']");
+         aList.children(".toc-text").addClass('actived');
+  }
+});
 
 
 
